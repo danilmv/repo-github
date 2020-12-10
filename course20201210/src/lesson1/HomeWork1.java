04e999328a4fd488b3a7518fe7a57197ca2145df
danilmv <danilmv+git@mail.ru>
2020-12-11 00:05:26 +0300

    added fourth method

--- course20201210/src/lesson1/HomeWork1.java
+++ course20201210/src/lesson1/HomeWork1.java
@@ -5,6 +5,7 @@ public class HomeWork1 {
         System.out.println("Результат первого метода: " + doCalc( 5, 3, 7, 9) );
         System.out.println("Результат второго метода: " + checkSum( 7, 3 ) );
         checkPositive( -7 );
+        sayHello("Вася");
     }
     public static float doCalc(int a, int b, int c, int d) {
         return a * (b + (c / (float)d));
@@ -17,5 +18,9 @@ public class HomeWork1 {
 
     public static void checkPositive(int a){
         System.out.println( "Число " + a + ( a >=0 ? " положительное" : " отрицательное" ));
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Привет, " + name + "!");
     }
 }
