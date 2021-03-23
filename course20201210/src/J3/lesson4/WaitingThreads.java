41e9d4b4959576b6a61e4f6ee7e6e8ebb647dec7
danilmv <43599357+danilmv@users.noreply.github.com>
2021-03-23 19:46:06 +0300

    J3HW4_p1 Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.

---
+++ course20201210/src/J3/lesson4/WaitingThreads.java
@@ -0,0 +1,46 @@
+package J3.lesson4;
+
+public class WaitingThreads implements Runnable {
+    private static final Object SYNC_MONITOR = new Object();
+    private static final int REPEATS = 5;
+
+    private volatile static char lastSymbol;
+
+    private final char symbol;
+    private final char previousSymbol;
+
+    public WaitingThreads(char symbol, char previousSymbol) {
+        this.symbol = symbol;
+        this.previousSymbol = previousSymbol;
+    }
+
+    @Override
+    public void run() {
+        synchronized (SYNC_MONITOR) {
+            for (int i = 0; i < REPEATS; i++) {
+                if (lastSymbol == previousSymbol) {
+                    System.out.print(symbol);
+                    lastSymbol = symbol;
+                } else
+                    i--;
+
+                SYNC_MONITOR.notifyAll();
+
+                try {
+                    SYNC_MONITOR.wait();
+                } catch (InterruptedException e) {
+                    System.err.println(Thread.currentThread().getName() + ": " + e.getMessage());
+                }
+            }
+            SYNC_MONITOR.notifyAll();
+        }
+    }
+
+    public static void main(String[] args) {
+        lastSymbol = 'C';
+
+        new Thread(new WaitingThreads('B', 'A')).start();
+        new Thread(new WaitingThreads('A', 'C')).start();
+        new Thread(new WaitingThreads('C', 'B')).start();
+    }
+}
