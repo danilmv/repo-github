2934d1e858c9a6ad29c236465a990e9168c4847a
danilmv <43599357+danilmv@users.noreply.github.com>
2021-03-26 00:27:19 +0300

    J3HW5 исправленный код

--- course20201210/src/J3/lesson5/MainClass.java
+++ course20201210/src/J3/lesson5/MainClass.java
@@ -1,13 +1,10 @@
 package J3.lesson5;
 
 import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    public static final int CARS_COUNT = 5;
 
     public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
         CyclicBarrier prepareCount = new CyclicBarrier(CARS_COUNT + 1);
