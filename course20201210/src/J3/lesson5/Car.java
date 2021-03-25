ff3b78029a81d35337fca78067444be0be99c8fc
danilmv <43599357+danilmv@users.noreply.github.com>
2021-03-26 00:40:04 +0300

    J3HW5 исправленный код

--- course20201210/src/J3/lesson5/Car.java
+++ course20201210/src/J3/lesson5/Car.java
@@ -6,8 +6,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private volatile static AtomicBoolean WIN = new AtomicBoolean(false);
-    private final CyclicBarrier raceCount;
-    private final CyclicBarrier prepareCount;
+    private final CyclicBarrier startCount;
+    private final CyclicBarrier announcementCount;
 
     static {
         CARS_COUNT = 0;
@@ -25,13 +25,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed, CyclicBarrier prepareCount, CyclicBarrier raceCount) {
+    public Car(Race race, int speed, CyclicBarrier announcement, CyclicBarrier startCount) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
-        this.raceCount = raceCount;
-        this.prepareCount = prepareCount;
+        this.startCount = startCount;
+        this.announcementCount = announcement;
     }
 
     @Override
@@ -40,16 +40,16 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
-            prepareCount.await();
+            announcementCount.await();
 
-            raceCount.await();
+            startCount.await();
             for (int i = 0; i < race.getStages().size(); i++) {
                 race.getStages().get(i).go(this);
             }
             if (WIN.compareAndSet(false, true)) {
                 System.out.println("\u001B[34m" + this.name + " - WIN\u001B[0m");
             }
-            prepareCount.await();
+            announcementCount.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- course20201210/src/J3/lesson5/MainClass.java
+++ course20201210/src/J3/lesson5/MainClass.java
@@ -4,24 +4,24 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
-    public static final int CARS_COUNT = 5;
+    public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
-        CyclicBarrier prepareCount = new CyclicBarrier(CARS_COUNT + 1);
-        CyclicBarrier raceCount = new CyclicBarrier(CARS_COUNT + 1);
+        CyclicBarrier announcementCount = new CyclicBarrier(CARS_COUNT + 1);
+        CyclicBarrier startCount = new CyclicBarrier(CARS_COUNT + 1);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), prepareCount, raceCount);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), announcementCount, startCount);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        prepareCount.await();
+        announcementCount.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        raceCount.await();
-        prepareCount.await();
+        startCount.await();
+        announcementCount.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
